d7ab278e7af4026e4f24cd1d9ab1c91480ef9694
Shrest2k5 <shrestshaw2k5@gmail.com>
2026-08-29 16:10:05 +0530

    making app and server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
-console.log("Backend server is running!");
+import dotenv from "dotenv";
+import app from "./app.js";
+import logger from "./utils/logger.js";
+import connectDB from "./db/connectDB.js";
+import redisClient from "./utils/redis.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        try {
+            await redisClient.connect();
+            logger.info("Redis Connected");
+        } catch (error) {
+            logger.warn("Redis connection failed. Continuing without Redis.");
+        }
+
+        app.listen(PORT, () => {
+            logger.info(`Server is running on ${PORT}`);
+        });
+
+    } catch (error) {
+        logger.error(error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
